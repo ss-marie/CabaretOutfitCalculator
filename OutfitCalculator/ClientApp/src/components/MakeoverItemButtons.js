83eeca24dc68104b1ce765f00bcaa18151527ff9
ss-marie <susan12@umbc.edu>
2021-01-08 10:46:50 -0500

    refactor to merge item vars into one array

--- OutfitCalculator/ClientApp/src/components/MakeoverItemButtons.js
+++ OutfitCalculator/ClientApp/src/components/MakeoverItemButtons.js
@@ -1,66 +1,21 @@
-﻿import React, { useState, useEffect, useCallback } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import { ToggleButtonGroup, ToggleButton, Button } from 'react-bootstrap';
 import StatDisplay from './StatDisplay';
 
 const MakeoverItemButtons = (makeoverItems) => {
-    const [hostessValue, setHostessValue] = useState('1');
-    const [dressValue, setDressValue] = useState('1');
-    const [hairstyleValue, setHairstyleValue] = useState('1');
-    const [hairAccessoryValue, setHairAccessoryValue] = useState('1');
-    const [eyeglassesValue, setEyeglassesValue] = useState('1');
-    const [earringsValue, setEarringsValue] = useState('1');
-    const [necklaceValue, setNecklaceValue] = useState('1');
-    const [nailsValue, setNailsValue] = useState('1');
-    const [ringValue, setRingValue] = useState('1');
-    const [watchValue, setWatchValue] = useState('1');
-    const [braceletValue, setBraceletValue] = useState('1');
-    const [perfumeValue, setPerfumeValue] = useState('1');
-    const [statsValue, setStatsValue] = useState('1');
-
-    const getSelectedItems = useCallback(() => [
-        hostessValue,
-        dressValue,
-        hairstyleValue,
-        hairAccessoryValue,
-        eyeglassesValue,
-        earringsValue,
-        necklaceValue,
-        nailsValue,
-        ringValue,
-        watchValue,
-        braceletValue,
-        perfumeValue,
-    ], [
-        hostessValue,
-        dressValue,
-        hairstyleValue,
-        hairAccessoryValue,
-        eyeglassesValue,
-        earringsValue,
-        necklaceValue,
-        nailsValue,
-        ringValue,
-        watchValue,
-        braceletValue,
-        perfumeValue
-    ]);
+    const [outfitValue, setOutfitValue] = useState([]);
+    const [statsValue, setStatsValue] = useState('');
 
     useEffect(() => {
         async function getStats() {
-            var selectedItems = getSelectedItems();
             var idList = "";
-            var counter = 0;
-            for (var i = 0; i < selectedItems.length; i++) {
-                var item = JSON.parse(selectedItems[i]);
+            for (var i = 0; i < outfitValue.length; i++) {
+                var item = outfitValue[i];
                 if (item.id) {
-                    if (counter > 0) {
-                        idList += "&";
-                    }
-                    idList += ("itemId=" + item.id);
-                    counter++;
+                    idList += ("&itemId=" + item.id);
                 }
             }
-            await fetch(`https://localhost:44330/outfit?${idList}&getBest=false`)
+            await fetch(`https://localhost:44330/outfit?getBest=false${idList}`)
                 .then((response) => response.json())
                 .then(data => {
                     setStatsValue(data.stats);
@@ -68,34 +23,7 @@ const MakeoverItemButtons = (makeoverItems) => {
                 });
         }
         getStats();
-    }, [
-        getSelectedItems,
-        hostessValue,
-        dressValue,
-        hairstyleValue,
-        hairAccessoryValue,
-        eyeglassesValue,
-        earringsValue,
-        necklaceValue,
-        nailsValue,
-        ringValue,
-        watchValue,
-        braceletValue,
-        perfumeValue
-    ]);
-
-    const hostess = 0;
-    const dress = 1;
-    const hairstyle = 2;
-    const hairAccessory = 3;
-    const eyeglasses = 4;
-    const earrings = 5;
-    const necklace = 6;
-    const nails = 7;
-    const ring = 8;
-    const watch = 9;
-    const bracelet = 10;
-    const perfume = 11;
+    }, [outfitValue]);
 
     var makeoverItemTypes = [
         "Hostess",
@@ -112,89 +40,47 @@ const MakeoverItemButtons = (makeoverItems) => {
         "Perfume"
     ]
 
-    const setItem = (value) => {
-        var makeoverItem = JSON.parse(value);
-        switch (makeoverItem.slot) {
-            case hostess: setHostessValue(value); break;
-            case dress: setDressValue(value); break;
-            case hairstyle: setHairstyleValue(value); break;
-            case hairAccessory: setHairAccessoryValue(value); break;
-            case eyeglasses: setEyeglassesValue(value); break;
-            case earrings: setEarringsValue(value); break;
-            case necklace: setNecklaceValue(value); break;
-            case nails: setNailsValue(value); break;
-            case ring: setRingValue(value); break;
-            case watch: setWatchValue(value); break;
-            case bracelet: setBraceletValue(value); break;
-            case perfume: setPerfumeValue(value); break;
-            default: break;
-        }
+    const setItem = (makeoverItemJson) => {
+        var makeoverItem = JSON.parse(makeoverItemJson);
+        var outfit = [...outfitValue];
+        outfit[makeoverItem.slot] = makeoverItem;
+        setOutfitValue(outfit);
     }
 
-    const getItem = (slot) => {
-        var item;
-        switch (slot) {
-            case makeoverItemTypes[hostess]: item = hostessValue; break;
-            case makeoverItemTypes[dress]: item = dressValue; break;
-            case makeoverItemTypes[hairstyle]: item = hairstyleValue; break;
-            case makeoverItemTypes[hairAccessory]: item = hairAccessoryValue; break;
-            case makeoverItemTypes[eyeglasses]: item = eyeglassesValue; break;
-            case makeoverItemTypes[earrings]: item = earringsValue; break;
-            case makeoverItemTypes[necklace]: item = necklaceValue; break;
-            case makeoverItemTypes[nails]: item = nailsValue; break;
-            case makeoverItemTypes[ring]: item = ringValue; break;
-            case makeoverItemTypes[watch]: item = watchValue; break;
-            case makeoverItemTypes[bracelet]: item = braceletValue; break;
-            case makeoverItemTypes[perfume]: item = perfumeValue; break;
-            default: break;
-        }
-        return item;
+    const getItem = (slotString) => {
+        var slotIndex = makeoverItemTypes.indexOf(slotString);
+        return JSON.stringify(outfitValue[slotIndex]);
     }
 
     async function getBestOutfit() {
-        var selectedItems = getSelectedItems();
         var idList = "";
-        var counter = 0;
-        for (var i = 0; i < selectedItems.length; i++) {
-            var item = JSON.parse(selectedItems[i]);
+        for (var i = 0; i < outfitValue.length; i++) {
+            var item = JSON.parse(outfitValue[i]);
             if (item.id) {
-                if (counter > 0) {
-                    idList += "&";
-                }
-                idList += ("itemId=" + item.id);
-                counter++;
+                idList += ("&itemId=" + item.id);
             }
         }
-        const response = await fetch(`https://localhost:44330/outfit?${idList}&getBest=true`)
+        const response = await fetch(`https://localhost:44330/outfit?getBest=true${idList}`)
         const outfit = await response.json();
 
-        setHostessValue(JSON.stringify(outfit.hostess));
-        setDressValue(JSON.stringify(outfit.dress));
-        setHairstyleValue(JSON.stringify(outfit.hairstyle));
-        setHairAccessoryValue(JSON.stringify(outfit.hairAccessory));
-        setEyeglassesValue(JSON.stringify(outfit.eyeglasses));
-        setEarringsValue(JSON.stringify(outfit.earrings));
-        setNecklaceValue(JSON.stringify(outfit.necklace));
-        setNailsValue(JSON.stringify(outfit.nails));
-        setRingValue(JSON.stringify(outfit.ring));
-        setWatchValue(JSON.stringify(outfit.watch));
-        setBraceletValue(JSON.stringify(outfit.bracelet));
-        setPerfumeValue(JSON.stringify(outfit.perfume));
+        setOutfitValue([
+            JSON.stringify(outfit.hostess),
+            JSON.stringify(outfit.dress),
+            JSON.stringify(outfit.hairstyle),
+            JSON.stringify(outfit.hairAccessory),
+            JSON.stringify(outfit.eyeglasses),
+            JSON.stringify(outfit.earrings),
+            JSON.stringify(outfit.necklace),
+            JSON.stringify(outfit.nails),
+            JSON.stringify(outfit.ring),
+            JSON.stringify(outfit.watch),
+            JSON.stringify(outfit.bracelet),
+            JSON.stringify(outfit.perfume)
+        ]);
     }
 
     const reset = () => {
-        setHostessValue(1);
-        setDressValue(1);
-        setHairstyleValue(1);
-        setHairAccessoryValue(1);
-        setEyeglassesValue(1);
-        setEarringsValue(1);
-        setNecklaceValue(1);
-        setNailsValue(1);
-        setRingValue(1);
-        setWatchValue(1);
-        setBraceletValue(1);
-        setPerfumeValue(1);
+        setOutfitValue([]);
     }
 
     return (
